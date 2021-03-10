6ace115204aa329a2156e41473c368bbe13a171e
Mr-strlen <cauitexp162carl@163.com>
2021-03-10 13:33:17 +0800

    基本完成查询功能，完成查询嵌套的子页面：查看员工信息，删除员工信息

--- app.py
+++ app.py
@@ -168,18 +168,54 @@ def StaffAdd():
 # 2.2 档案查询 - 员工群落
 @app.route("/staff_list",methods=['GET', 'POST'])
 def StaffList():
-    if request.method == 'POST':
-        name = request.form.get("username")
-        temp = db.session.query(Staff_Info).filter(Staff_Info.Staff_Identify == name).all()
-        for v in temp:
-            print(v.Staff_Name)
-        return render_template("staff_list.html",rangeid=0,page_data=temp)
-    return render_template("staff_list.html")
+    name = request.form.get("username", '', str)
+    degree = request.form.get("degree", '', str)
+    offset = request.args.get('offset', 0, int)
+    limit = request.args.get('limit', 10, int)
+
+    search_str = ""
+    # 属性是否为空判断，拼接搜索条件
+    if len(name) > 0:
+        search_str = search_str + "Staff_Info.Staff_Identify == name,"
+    if len(degree) > 0:
+        search_str = search_str + "Staff_Info.Staff_Degree == degree,"
+
+    # 如果搜索条件为空，即为全部搜索
+    temp = []
+    if len(search_str) > 0:
+        search_str = search_str[0:-1] # 逗号删减
+        temp = eval("db.session.query(Staff_Info).filter(" + search_str + ").order_by(Staff_Info.Staff_Identify).limit(limit).offset(offset).all()")
+        count = len(eval("db.session.query(Staff_Info).filter(" + search_str + ").all()"))
+    else:
+        temp = db.session.query(Staff_Info).order_by(Staff_Info.Staff_Identify).limit(limit).offset(offset).all()
+        count = len(db.session.query(Staff_Info).all())
+
+    for v in temp:
+        print(v.Staff_Name)
+
+    return render_template("staff_list.html", page_data=temp, rangeid=0, offset=offset, limit=limit, count=count)
+
+# 嵌套在页面内 员工个人档案查看
+@app.route("/staff_look/<id>")
+def StaffLook(id):
+    temp = db.session.query(Staff_Info).filter(Staff_Info.Staff_Identify == id).all()
+    return render_template("staff_look.html", person=temp[0])
+
+# 嵌套在页面内 员工个人删除查看
+@app.route("/staff_del/<id>",methods=['GET', 'POST'])
+def StaffDel(id):
+    print(id)
+    temp=db.session.query(Staff_Info).filter(Staff_Info.Staff_Identify == id).first()
+    print(temp)
+    db.session.delete(temp)
+    db.session.commit()
+    data="1"
+    return data
 
 # 2.3 档案修改（勘误）
 @app.route("/staff_change")
 def StaffChange():
-    return render_template("staff_change.html")
+    return render_template("staff_change.html",)
 
 # 2.4 档案修改（离职） - 员工离职
 @app.route("/staff_leave")
